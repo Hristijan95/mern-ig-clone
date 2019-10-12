b8f05f00d23c4055a892b709b8d8fa1ac22a35d6
Hristijan Ilieski <ilieski.hristijan@gmail.com>
2019-10-12 12:01:34 +0200

    User Login & Register

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is listenenig on port ${PORT}`));
